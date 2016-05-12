b46812dc5ab968cd4ed2be7c9797a50cbca0a4c1
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-05-12 12:50:32 +0200

    Init

---
+++ library/JSONPointer/Aeson/Interpreter.hs
@@ -0,0 +1,27 @@
+module JSONPointer.Aeson.Interpreter where
+
+import BasePrelude
+import qualified JSONPointer.Model
+import qualified Data.Aeson as Aeson
+import qualified Data.HashMap.Strict as HashMap.Strict
+import qualified Data.Vector as Vector
+
+
+-- |
+-- Converts JSONPointer into an Aeson Value lookup function.
+value :: JSONPointer.Model.JSONPointer -> Aeson.Value -> Maybe Aeson.Value
+value pointer json =
+  appEndo (JSONPointer.Model.run pointer interpreter) $
+  Just json
+  where
+    interpreter index key =
+      Endo ((=<<) lookup)
+      where
+        lookup =
+          \case
+            Aeson.Object x ->
+              HashMap.Strict.lookup key x
+            Aeson.Array x ->
+              (Vector.!?) x =<< index
+            _ ->
+              Nothing
